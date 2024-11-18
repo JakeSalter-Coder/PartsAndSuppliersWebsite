d2d67af08331c5dc7c9a1b93a215b364a1b4fda3
JakeSalter-Coder <jsalty15@gmail.com>
2024-11-18 13:26:38 -0600

    Template added

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'superpie1'
+mysql = MySQL(app)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="localhost", port=4560, debug=True)
